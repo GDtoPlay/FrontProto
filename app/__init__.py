bdfb8eee639ced523a4020914f9db5e058a42d62
key13689 <971215pjs@naver.com>
2018-07-21 12:23:17 -0700

    apply flask-bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from flask import Flask, request, redirect, url_for
 from config import Config
 from werkzeug import secure_filename
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 
 UPLOAD_FOLDER = '/home/tozero13689/FrontProto/app/upload/test'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'pcap'])
 
 app = Flask(__name__)
+Bootstrap(app)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config.from_object(Config)
 
